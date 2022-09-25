8f2faeebe60b33609d3246a5bc9af9c42802157e
Archit669 <architjain669@gmail.com>
2022-09-25 23:54:52 +0530

    add login page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import Resume from "./components/Resume";
 import "./scss/style.scss";
 import "./index.css";
 import Landing from "./components/Landing"
+import Login from "./components/Login";
 
 function App() {
   return (
     <div>
-      <Landing />
+      {/* <Landing /> */}
       {/* <Resume /> */}
+      <Login />
     </div>
   );
 }
---
+++ client/src/components/Login.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <div className="main">
+            <div className="logo"></div>
+            <div className="card">
+                <div className="title"> Resume Builder</div>
+                <form>
+                    <div className="credentials">
+                        <div className="username">
+                            <span className="glyphicon glyphicon-user"></span>
+                            <input type="text" name="username" placeholder="Username" required/>
+                        </div>
+                        <div className="password">
+                            <span className="glyphicon glyphicon-lock"></span>
+                            <input type="text" password name="password" placeholder="Password" required/>
+                        </div>
+                    </div>
+                    <button className="submit">Submit</button>
+                </form>
+                <div className="link">
+                    <a href="#">Forget Password</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="#">Sign Up</a>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
