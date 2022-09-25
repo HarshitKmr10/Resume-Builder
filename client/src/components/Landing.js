a98dbf0ed67111e6ed92872a0d81727ea356409d
Chaitanya134 <chaitanyagiri2@gmail.com>
2022-09-26 00:08:17 +0530

    Added Routes

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,31 +1,37 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom"
 
 function Landing() {
-  return (
-    <div className='header'>
-        <div className="navbar container">
-            <div className="logo">Resume Builder</div>
-            <ul className="nav-items">
-                <div className="sign-in btn btn-secondary">Log in</div>
-                <div className="getting-started-today btn btn-primary">Get started today</div>
-            </ul>
-        </div>
 
-        <div className="main-content">
-            <div className="main-intro">
-                <h1>Build your <span>QR Code Based</span> resume from scratch</h1>
-            </div>
+	const navigate = useNavigate();
 
-            <div className="secondary-intro">
-                <p>Most QR code based resume building software are accurate, but hard to use. We make it user-friendly and easy to use.</p>
-            </div>
+	return (
+		<div className='header'>
+			<div className="navbar container">
+				<div className="logo">Resume Builder</div>
+				<ul className="nav-items">
+					<div className="sign-in btn btn-secondary" onClick={() => navigate("/login")}>Log in</div>
+					<div className="getting-started-today btn btn-primary" onClick={() => navigate("signup")}>
+						Get started today
+					</div>
+				</ul>
+			</div>
 
-            <div className="btn btn-primary">Build your resume</div>
-            <div className="btn btn-secondary">Browse Templates</div>
-        </div>
-        
-    </div>
-  )
+			<div className="main-content">
+				<div className="main-intro">
+					<h1>Build your <span>QR Code Based</span> resume from scratch</h1>
+				</div>
+
+				<div className="secondary-intro">
+					<p>Most QR code based resume building software are accurate, but hard to use. We make it user-friendly and easy to use.</p>
+				</div>
+
+				<div className="btn btn-primary" onClick={() => navigate("/chaitanyagiri/1")}>Build your resume</div>
+				<div className="btn btn-secondary" onClick={() => navigate("/templates")}>Browse Templates</div>
+			</div>
+
+		</div>
+	)
 }
 
 export default Landing
